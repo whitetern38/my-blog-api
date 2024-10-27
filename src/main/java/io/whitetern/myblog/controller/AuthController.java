fd7b784ac25df05b82ced9bd4e6ba8ee72f2ecbd
whitetern <whitetern38@gmail.com>
2024-10-27 18:36:52 +0900

    [Auth] auth 컨트롤러 서비스단 추가

---
+++ src/main/java/io/whitetern/myblog/controller/AuthController.java
@@ -0,0 +1,39 @@
+package io.whitetern.myblog.controller;
+
+import io.whitetern.myblog.domain.User;
+import io.whitetern.myblog.dto.auth.RequestLoginDto;
+import io.whitetern.myblog.service.AuthService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(
+            @RequestBody @Valid RequestLoginDto requestLoginDto,
+            HttpSession session
+    ) {
+        User user = authService.login(requestLoginDto);
+        session.setAttribute("user", user);
+
+        return ResponseEntity.ok("로그인 성공");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpSession session) {
+        session.removeAttribute("user");
+        return ResponseEntity.ok("로그아웃 성공");
+    }
+
+}
---
+++ src/main/java/io/whitetern/myblog/service/AuthService.java
@@ -0,0 +1,29 @@
+package io.whitetern.myblog.service;
+
+import io.whitetern.myblog.domain.User;
+import io.whitetern.myblog.dto.auth.RequestLoginDto;
+import io.whitetern.myblog.exception.AuthException;
+import io.whitetern.myblog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User login(RequestLoginDto requestLoginDto) {
+        User user = userRepository.findByLoginId(requestLoginDto.getLoginId())
+                .orElseThrow(() -> new AuthException("해당 아이디로 가입된 정보가 없습니다."));
+
+        if (!passwordEncoder.matches(requestLoginDto.getPassword(), user.getPassword())) {
+            throw new AuthException("비밀번호가 일치하지 않습니다.");
+        }
+
+        return user;
+    }
+
+}
