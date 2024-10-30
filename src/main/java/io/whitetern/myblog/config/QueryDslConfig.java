37b2813d33bec10186eaab238aa162a0cfe30c04
whitetern <whitetern38@gmail.com>
2024-10-30 22:19:42 +0900

    [REFACTORING] QueryDsl Config 수정

--- src/main/java/io/whitetern/myblog/config/QueryDslConfig.java
+++ src/main/java/io/whitetern/myblog/config/QueryDslConfig.java
@@ -2,14 +2,18 @@ package io.whitetern.myblog.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QueryDslConfig {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @Bean
-    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+    public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
     }
 
